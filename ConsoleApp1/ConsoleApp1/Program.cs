0681b2b5cb29517ceda47ab159ed6b5b543aa1ce
MariaZuevaMouse <58681283+MariaZuevaMouse@users.noreply.github.com>
2020-06-27 13:08:07 +0300

    class

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,24 +10,21 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Snake!");
-            int x1 = 1;
-            int y1 = 3;
-            char symbol1 = '*';
+            Point p1 = new Point();
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+            p1.Draw();
 
-            Console.SetCursorPosition(x1, y1);
-            Console.Write(symbol1);
-
-            int x2 = 4;
-            int y2 = 5;
-            char symbol2 = '#';
-
-            Console.SetCursorPosition(x2, y2);
-            Console.Write(symbol2);
-
+            Point p2 = new Point();
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+            p2.Draw();
 
             Console.ReadKey();
 
         }
+   
     }
 }
