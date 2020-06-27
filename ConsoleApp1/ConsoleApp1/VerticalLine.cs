c34103d7a28aaff0a5228c53847274813b707585
MariaZuevaMouse <58681283+MariaZuevaMouse@users.noreply.github.com>
2020-06-27 22:38:05 +0300

    lines

---
+++ ConsoleApp1/ConsoleApp1/VerticalLine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class VerticalLine
+    {
+        List<Point> pList;
+
+        public VerticalLine(int yStart, int yLow, int x, char sym)
+        {
+            pList = new List<Point>();
+            for (int y = yStart; y <= yLow; y++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+        }
+        public void Draw()
+        {
+            foreach(Point p in pList)
+            {
+                p.Draw();
+            }
+        }
+    }
+}
