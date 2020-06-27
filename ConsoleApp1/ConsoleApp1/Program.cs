f69fca4134c0b6b5517fbed2eba2a6cb9682dfa3
MariaZuevaMouse <58681283+MariaZuevaMouse@users.noreply.github.com>
2020-06-27 12:16:59 +0300

    function

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,21 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Snake!");
+            int x1 = 1;
+            int y1 = 3;
+            char symbol1 = '*';
+
+            Console.SetCursorPosition(x1, y1);
+            Console.Write(symbol1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char symbol2 = '#';
+
+            Console.SetCursorPosition(x2, y2);
+            Console.Write(symbol2);
+
+
             Console.ReadKey();
 
         }
