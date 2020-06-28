172b55dc64379bf99b1024e483f6fbdc2fff6a2b
MariaZuevaMouse <58681283+MariaZuevaMouse@users.noreply.github.com>
2020-06-28 14:35:41 +0300

    snake eat

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -25,22 +25,34 @@ namespace ConsoleApp1
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
             
 
             while (true)
             {
-                if(Console.KeyAvailable)
+                if(snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                }
+                else
+                {
+                    snake.Move();
+                }
+                Thread.Sleep(300);
+
+                if (Console.KeyAvailable)
                 {
                     ConsoleKeyInfo key = Console.ReadKey();
                     snake.HandleKey(key.Key);
                 }
-                Thread.Sleep(100);
-                snake.Move();
             }
 
 
 
-            Console.ReadKey();
+           // Console.ReadKey();
       
         }
     }
