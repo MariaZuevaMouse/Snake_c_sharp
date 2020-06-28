c842a9edd5c33899dd52c1ebf8da496820b97dd4
MariaZuevaMouse <58681283+MariaZuevaMouse@users.noreply.github.com>
2020-06-28 16:18:06 +0300

    snake game

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -13,14 +13,16 @@ namespace ConsoleApp1
         {
             //Console.SetBufferSize(80, 24);
                      
-            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
+           /* HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
             HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
             VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '$');
+            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
             upline.Draw();
             downline.Draw();
             leftline.Draw();
-            rightLine.Draw();
+            rightLine.Draw();*/
+            Wall walls = new Wall(80, 25);
+            walls.Draw();
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
@@ -33,15 +35,20 @@ namespace ConsoleApp1
 
             while (true)
             {
-                if(snake.Eat(food))
+                if(walls.IsHit(snake) || snake.IsHitTail() )
+                {
+                    break;
+                }
+                if (snake.Eat(food))
                 {
                     food = foodCreator.CreateFood();
+                    food.Draw();
                 }
                 else
                 {
                     snake.Move();
                 }
-                Thread.Sleep(300);
+                Thread.Sleep(150);
 
                 if (Console.KeyAvailable)
                 {
---
+++ ConsoleApp1/ConsoleApp1/Wall.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Wall
+    {
+        List<Figure> wallList;
+
+        public Wall(int mapWidth, int mapHeight)
+        {
+            wallList = new List<Figure>();
+
+            HorizontalLine upline = new HorizontalLine(0, mapWidth -2, 0, '+');
+            HorizontalLine downline = new HorizontalLine(0, mapWidth -2, mapHeight - 1, '+');
+            VerticalLine leftline = new VerticalLine(0, mapHeight - 1, 0, '+');
+            VerticalLine rightLine = new VerticalLine(0, mapHeight - 1, mapWidth - 2, '+');
+
+            wallList.Add(upline);
+            wallList.Add(downline);
+            wallList.Add(leftline);
+            wallList.Add(rightLine);
+        }
+        internal bool IsHit(Figure figure)
+        {
+            foreach(var wall in wallList)
+            {
+                if(wall.IsHit(figure))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public void Draw()
+        {
+            foreach (var wall in wallList)
+            {
+                wall.Draw();
+            }
+        }
+    }
+}
